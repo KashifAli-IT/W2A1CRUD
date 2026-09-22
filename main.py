2bb0734cc1a9b7b5f2a471db39da350338427a19
Kashif Ali <146372614+KashifAli-IT@users.noreply.github.com>
2026-09-22 13:38:16 +0500

    stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
 app = FastAPI()
+
+# In-memory storage for tasks
+tasks = [
+    {
+        "id": 1,
+        "title": "Learn FastAPI",
+        "completed": False
+    },
+    {
+        "id": 2,
+        "title": "Build CRUD API",
+        "completed": False
+    },
+    {
+        "id": 3,
+        "title": "Push Project to GitHub",
+        "completed": False
+    }
+]
 
 @app.get("/")
 def read_root():
@@ -14,3 +35,16 @@ def read_health():
     return {
         "status": "ok"
         }
+
+@app.get("/tasks/{id}")
+def get_task(id: int):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+   
+    return JSONResponse(
+        status_code=404,
+        content={
+            "error": f"Task with ID {id} not found"
+            }
+        )        
